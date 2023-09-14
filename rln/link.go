87caaeb9e6c9f4b725df032756a4cbc9a2d478a2
Richard Ramos <info@richardramos.me>
2023-09-14 19:06:37 -0400

    feat: upgrade to zerokit 0.3.4

--- rln/link.go
+++ rln/link.go
@@ -1,7 +1,7 @@
 package rln
 
 /*
-#cgo LDFLAGS:-lrln -ldl -lm
+#cgo LDFLAGS: -lrln -ldl -lm
 #cgo darwin,386,!ios LDFLAGS:-L${SRCDIR}/../libs/i686-apple-darwin
 #cgo darwin,arm64,!ios LDFLAGS:-L${SRCDIR}/../libs/aarch64-apple-darwin
 #cgo darwin,amd64,!ios LDFLAGS:-L${SRCDIR}/../libs/x86_64-apple-darwin
