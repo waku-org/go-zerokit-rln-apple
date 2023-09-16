ee0ee61dde2b4e793b60b9e9cc71cbafeb2217f6
Richard Ramos <info@richardramos.me>
2023-09-16 13:23:09 -0400

    chore: add reference to library folders for go mod vendor

--- rln/link.go
+++ rln/link.go
@@ -1,5 +1,10 @@
 package rln
 
+import (
+	_ "github.com/waku-org/go-zerokit-rln-apple/libs/aarch64-apple-darwin"
+	_ "github.com/waku-org/go-zerokit-rln-apple/libs/x86_64-apple-darwin"
+)
+
 /*
 #cgo LDFLAGS: -lrln -ldl -lm
 #cgo darwin,386,!ios LDFLAGS:-L${SRCDIR}/../libs/i686-apple-darwin
