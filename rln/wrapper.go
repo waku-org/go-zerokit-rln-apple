7fc220de3e740353b597885123dc60507b0189df
alrevuelta <alvrevuelta@gmail.com>
2024-01-11 17:39:50 +0100

    Bump zerokit to include proof with witness

--- rln/wrapper.go
+++ rln/wrapper.go
@@ -159,6 +159,20 @@ func (r *RLN) GenerateRLNProof(input []byte) ([]byte, error) {
 	return C.GoBytes(unsafe.Pointer(out.ptr), C.int(out.len)), nil
 }
 
+func (r *RLN) GenerateRLNProofWithWitness(input []byte, witness []byte) ([]byte, error) {
+	inputBuffer := toCBufferPtr(input)
+	witnessBuffer := toCBufferPtr(witness)
+
+	var output []byte
+	out := toBuffer(output)
+
+	if !bool(C.generate_rln_proof_with_witness(r.ptr, inputBuffer, witnessBuffer, &out)) {
+		return nil, errors.New("could not generate the proof with witness")
+	}
+
+	return C.GoBytes(unsafe.Pointer(out.ptr), C.int(out.len)), nil
+}
+
 func (r *RLN) VerifyWithRoots(input []byte, roots []byte) (bool, error) {
 	proofBuf := toCBufferPtr(input)
 	rootBuf := toCBufferPtr(roots)
